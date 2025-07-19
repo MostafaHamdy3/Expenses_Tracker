b0c7e31601c5ccf950263b5c279938b0e0931451
Mostafa Hamdy <73959716+MostafaHamdy3@users.noreply.github.com>
2025-07-19 18:44:42 +0300

    fix: GestureDetector

--- App.tsx
+++ App.tsx
@@ -5,18 +5,21 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import AppNavigation from "./app/AppNavigation";
 import { FontLoader } from "./app/constants/FontLoader";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <FontLoader>
-          <StatusBar style="dark" />
-          <BottomSheetModalProvider>
-            <AppNavigation />
-          </BottomSheetModalProvider>
-        </FontLoader>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <FontLoader>
+            <StatusBar style="dark" />
+            <BottomSheetModalProvider>
+              <AppNavigation />
+            </BottomSheetModalProvider>
+          </FontLoader>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
